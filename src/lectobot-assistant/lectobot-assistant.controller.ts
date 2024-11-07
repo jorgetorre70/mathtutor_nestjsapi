b9040350517defcd3b0e73facdf97a21a8fc88ce
jorgetorre70 <jorgetorre1022@gmail.com>
2024-11-07 07:48:43 -0600

    3rd CORS issue fixed

--- src/lectobot-assistant/lectobot-assistant.controller.ts
+++ src/lectobot-assistant/lectobot-assistant.controller.ts
@@ -3,6 +3,15 @@ import { LectobotAssistantService } from './lectobot-assistant.service';
 import { QuestionDto } from './dtos/question.dto';
 
 @Controller('lectobot-assistant')
+@UseCors({
+  origin: [
+    'https://main.d1p89hswv7ju6x.amplifyapp.com',
+    'https://aimathmentor.com',
+  ],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+})
 export class LectobotAssistantController {
   constructor(
     private readonly lectobotAssistantService: LectobotAssistantService,
@@ -15,4 +24,15 @@ export class LectobotAssistantController {
   @Post('user-question') async userQuestion(@Body() questionDto: QuestionDto) {
     return await this.lectobotAssistantService.userQuestion(questionDto);
   }
+}
+function UseCors(arg0: {
+  // Añade esta línea
+  origin: string[];
+  methods: string[];
+  allowedHeaders: string[];
+  credentials: boolean;
+}): (
+  target: typeof LectobotAssistantController,
+) => void | typeof LectobotAssistantController {
+  throw new Error('Function not implemented.');
 }
